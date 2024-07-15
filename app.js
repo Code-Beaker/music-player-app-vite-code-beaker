58d884563a98e12bf220a6ca4f7637655e759e9f
Tharun Raj <tharun.2.linux@gmail.com>
2024-07-15 15:24:15 +0530

    migrate to vite from parcel

---
+++ app.js
@@ -0,0 +1,25 @@
+import openIcon from "/assets/icon-menu.svg";
+import closeIcon from "/assets/icon-close.svg";
+
+const toggleSidebarButton = document.querySelector("#toggleSidebar");
+const toggleSidebarIcon = document.querySelector("#toggleSidebarIcon");
+const searchBtn = document.querySelector("#searchButton");
+const searchBar = document.querySelector("#searchForMusic");
+const trackInfo = document.querySelector(".navbar__music-details");
+let hidden = true;
+
+searchBtn.addEventListener("click", () => {
+  searchBar.classList.toggle("display");
+  trackInfo.classList.toggle("hide-content");
+  toggleSidebarButton.classList.toggle("hide-content");
+});
+
+toggleSidebarButton.addEventListener("click", () => {
+  if (hidden) {
+    toggleSidebarIcon.setAttribute("src", closeIcon);
+    hidden = false;
+  } else {
+    toggleSidebarIcon.setAttribute("src", openIcon);
+    hidden = true;
+  }
+});
